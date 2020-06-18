01bc58e3ea66089d1d91e1c8150450216b08bf28
Curtis <laurencechadwell@gmail.com>
2020-06-17 22:23:54 -0500

    Need to work on edit, delete and user

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -17,20 +17,60 @@ class PlayersController < ApplicationController
         end
     end
 
-    post "/players" do 
-       if params == ""
-         redirect to '/players/new'
-       else
-        @player = Player.create(params)
-        redirect to "/players/#{@player.id}"
-        
-       end
-    end
+    
 
     get "/players/:id" do
+      if logged_in?
         @player = Player.find(params[:id])
         erb :"players/show_player"
+     else
+       redirect to '/login' 
+     end
     end
 
+    get '/players/:id/edit' do  #load edit form
+        if logged_in?
+         @player = Player.find_by_id(params[:id])
+            if @player && @player.user == current_user
+            erb :"players/modify"
+            else 
+            redirect to '/players'
+            end
+        else
+            redirect to '/login'
+        end
+    end
 
+    patch '/players/:id/edit' do
+        @player = Player.find_by_id(params[:id])
+        @player.name = params[:name]
+        @player.position = params[:position]
+        @player.height = params[:height]
+        @player.weight = params[:weight]
+        @player.save
+        redirect to "/players/#{@player.id}"    
+        
+    end
+
+    post "/players" do 
+        if params == ""
+          redirect to '/players/new'
+        else
+         @player = Player.create(params)
+         redirect to "/players/#{@player.id}"
+         
+        end
+     end
+
+    delete '/players/:id' do #delete action
+      if logged_in?
+        @player = Player.find_by_id(params[:id])
+        if @player && @player.user == current_user
+        @player.delete
+        end
+            redirect to '/players'
+        else
+            redirect to '/login'
+        end
+    end
 end
