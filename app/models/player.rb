6c3aaecb1caa001cf2ad11b93e8b338d881a3069
Curtis <laurencechadwell@gmail.com>
2020-06-22 23:10:51 -0500

    still trying to figure out url slug

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,13 @@
 class Player <ActiveRecord::Base
     belongs_to :team
     has_many :coaches, through: :team
+
+      def slug
+        name.downcase.gsub(" ","-")
+      end
+    
+      def self.find_by_slug(slug)
+        Player.all.find{|player| player.slug == slug}
+      end
     
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -3,13 +3,7 @@ has_many :players
 has_many :coaches
 has_secure_password
 
-  def slug
-    username.downcase.gsub(" ","-")
-  end
-
-  def self.find_by_slug(slug)
-    User.all.find{|user| user.slug == slug}
-  end
+  
 
 
 end
