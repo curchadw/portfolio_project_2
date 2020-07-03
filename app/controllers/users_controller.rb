ffcfde314741405e1e8c4efa153cc48f30d3493c
Curtis <laurencechadwell@gmail.com>
2020-07-03 08:33:32 -0500

    Trying to validate username uniqueness to prevent duplication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,18 @@ class UsersController < ApplicationController
         if params[:username] == "" || params[:email] == "" || params[:password] == "" || params[:name] == ""
             flash[:notice] = "<h3 class ='alert'>Please fill-in info!</h3>"
             redirect to '/signup'
+        
+        
         else
+            if @user
+              flash[:notice] = "<h3>Username already exist, try anoter one.</h3>"
+              redirect to '/signup'
+            else
+              @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password], :name => params[:name])
+              session[:user_id] = @user.id
+              redirect to '/account'
             
-          @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password], :name => params[:name])
-          session[:user_id] = @user.id
-          redirect to '/account'
-          
+            end
         end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User <ActiveRecord::Base
     include Slugifiable::InstanceMethods
     extend Slugifiable::ClassMethods
-
+    validates_uniqueness_of :username, case_sensitive: false
     has_many :players
 
     has_secure_password
