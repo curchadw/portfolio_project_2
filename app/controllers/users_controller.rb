87ed3da682cdc2aac8cd8d04fb824bbedd2735a7
Curtis Laurence Chadwell <43461325+curchadw@users.noreply.github.com>
2020-07-08 13:06:11 -0500

    Update users_controller.rb
    Adjusted some indention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,33 +8,26 @@ class UsersController < ApplicationController
         if params[:username] == "" || params[:email] == "" || params[:password] == "" || params[:name] == ""
             flash[:notice] = "<h3 class ='alert'>Please fill-in info!</h3>"
             redirect to '/signup'
-        
-        
         else
-            
-              
            user = User.new(:username => params[:username], :email => params[:email], :password => params[:password], :name => params[:name])
-           if user.save
-            session[:user_id] = user.id
-            redirect to '/account'
+           
+            if user.save
+                session[:user_id] = user.id
+                redirect to '/account'
            else
-            flash[:notice] = "<h3>Username already exist, try another one.</h3>"
-            redirect to '/signup'
+                flash[:notice] = "<h3>Username already exist, try another one.</h3>"
+                redirect to '/signup'
            end
             
         end
     end
 
-
     
-
+    get '/account' do
+         redirect_if_not_logged_in
+         erb :'players/account'
       
-    
-      get '/account' do
-        redirect_if_not_logged_in
-        erb :'players/account'
-      
-      end
+    end
 
       
 
